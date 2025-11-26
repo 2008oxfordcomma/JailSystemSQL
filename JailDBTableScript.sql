81d1d4e62226bb1e6ce3d2e9ff47d951e0a3a319
Dre Harm <2008oxfordcomma@gmail.com>
2025-11-26 09:01:04 -0600

    Completed Table Creation
    Adjusted calculation logic, added additional constraints, added missing foreign keys

--- JailDBTableScript.sql
+++ JailDBTableScript.sql
@@ -3,7 +3,6 @@ drop database `Jail System`;
 create database `Jail System`;
 use `Jail System`;
 
--- complete 
 create table `visitor`(
 	`visitor_id` int primary key,
     `first_name` varchar(255) NOT NULL,
@@ -14,24 +13,6 @@ create table `visitor`(
 	`approved` boolean DEFAULT false
 );
 
--- AGE needs to be derived as view
-create table `inmate`(
-	`inmate_id` int primary key,
-    `cell_id` int,
-	`first_name` varchar(255) not null,
-    `last_name` varchar(255) not null,
-    `dob` date not null,
-    `gender` enum('M', 'F', 'Other') not null,
-    `admission_date` date not null,
-    `release_date` date,
-    `status` enum('In-Custody', 'Released', 'Transferred', 'Deceased') default 'In-Custody',
-	`security_level` enum('Low', 'Medium', 'High', 'Maximum') NOT NULL
-	-- `age` INT GENERATED ALWAYS AS (TIMESTAMPDIFF(year, dob, curdate())
-);
-
- -- insert into `inmate`(inmate_id, cell_id, first_name, last_name, dob, gender, admission_date, release_date, status, security_level) values(123, 12, 'bob', 'cat', '2000-01-01', 'M', '2023-03-24', '2025-01-01', 'In-Custody', 'Low');
- 
--- complete
 create table `staff`(
 	`staff_id` int primary key,
 	`first_name` varchar(255) not null,
@@ -42,48 +23,63 @@ create table `staff`(
     `email` varchar(255)
 );
 
--- SENTENCE_LENGTH NEEDS TO BE FIXED
+create table `block`(
+	`block_id` int primary key,
+    `name` varchar(255) NOT NULL UNIQUE,
+    `type` enum('Male', 'Female', 'Juvenile', 'Solitary') NOT NULL,
+	`capacity` INT NOT NULL CHECK (`capacity` > 0) NOT NULL,
+    supervisor_id INT,
+    
+    FOREIGN KEY (supervisor_id) REFERENCES staff(staff_id)
+);
+
+create table `cell`(
+	`cell_id` int NOT NULL,
+    `block_id` int NOT NULL,
+	`capacity` INT NOT NULL CHECK (`capacity` > 0),
+    `security_level` enum('Low', 'Medium', 'High', 'Maximum') NOT NULL,
+    
+    PRIMARY KEY (cell_id, block_id),
+    FOREIGN KEY (block_id) references block(block_id)
+);
+
+create table `inmate`(
+	`inmate_id` int primary key,
+    `cell_id` int,
+	`first_name` varchar(255) not null,
+    `last_name` varchar(255) not null,
+    `dob` date not null,
+    `gender` enum('M', 'F', 'Other') not null,
+    `admission_date` date not null,
+    `release_date` date,
+    `status` enum('In-Custody', 'Released', 'Transferred', 'Deceased') default 'In-Custody',
+	`security_level` enum('Low', 'Medium', 'High', 'Maximum') NOT NULL,
+    
+    FOREIGN KEY (cell_id) REFERENCES cell(cell_id)
+);
+
 create table `Offense`(
 	`offense_id` int primary key,
     `inmate_id` int not null,
     `charge` varchar(255) not null,
-    `court_case_number` date not null,
+    `court_case_number` varchar(255) not null,
     `start_date` date not null,
     `end_date` date not null,
     `parole_eligible` boolean default false,
-    `sentence_length` INT GENERATED ALWAYS AS (TIMESTAMPDIFF(day, end_date, start_date)),
-    foreign key (inmate_id) references inmate(`inmate_id`)
+    `sentence_length` INT GENERATED ALWAYS AS (TIMESTAMPDIFF(day, start_date, end_date)),
+    FOREIGN KEY (inmate_id) references inmate(inmate_id)
 );
 
--- complete 
-create table `Visit`(
+create table `visit`(
 	`visit_id` int primary key,
     `inmate_id` int NOT NULL,
     `visitor_id` int NOT NULL,
     `staff_id` int NOT NULL,
     `visit_date` datetime NOT NULL,
 	`duration_minutes` float,
-    foreign key (inmate_id) references inmate(`inmate_id`),
-    foreign key (visitor_id) references visitor(`visitor_id`),
-    foreign key (staff_id) references staff(`staff_id`)
-);
-
-
-create table `block`(
-	`block_id` int primary key,
-    `name` varchar(255) NOT NULL UNIQUE,
-    `type` enum('Male', 'Female', 'Juvenile', 'Solitary') NOT NULL,
-	`capacity` INT CHECK (`capacity` > 0) NOT NULL
-);
-
--- OCCUPANCY needs to be derived as view
-create table `cell`(
-	`cell_id` int,
-    `block_id` int,
-	`capacity` INT CHECK (`capacity` > 0) NOT NULL,
-    `security_level` enum('Low', 'Medium', 'High', 'Maximum') NOT NULL,
-
-    PRIMARY KEY (cell_id, block_id)
+    foreign key (inmate_id) references inmate(inmate_id),
+    foreign key (visitor_id) references visitor(visitor_id),
+    foreign key (staff_id) references staff(staff_id)
 );
 
 select @@version;

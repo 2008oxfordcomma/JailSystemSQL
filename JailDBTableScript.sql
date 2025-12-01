7afaf073592ef4406a65db302879a3e44d8a3929
Dre Harm <2008oxfordcomma@gmail.com>
2025-12-01 07:31:40 -0600

    Consolidated offense and inmate insert statements
    Corrected foreign keys for inmate

--- JailDBTableScript.sql
+++ JailDBTableScript.sql
@@ -270,94 +270,32 @@ create table `inmate`(
     `release_date` date,
     `status` enum('In-Custody', 'Released', 'Transferred', 'Deceased') default 'In-Custody',
 	`security_level` enum('Low', 'Medium', 'High', 'Maximum') NOT NULL,
-    FOREIGN KEY (cell_id) REFERENCES cell(cell_id),
-    foreign key (block_id) references block(block_id)
+    
+    FOREIGN KEY (block_id, cell_id) REFERENCES cell(block_id, cell_id)
 );
 
-insert into inmate values(
-	58148, 1, 1, 'Anthony', 'Arreola', '1987-04-20', 1, '2025-08-23', '2025-08-23', 1, 1
-);
-
-insert into inmate values(
-	376003, 1, 1, 'Benjamin', 'Blaschka', '1977-06-25', 1, '2025-11-21', '2026-11-21', 1, 1
-);
-
-insert into inmate values(
-	5540, 1, 2, 'Trisha', 'Fenske', '1974-09-13', 2, '2025-11-25', '2026-11-25', 1, 1
-);
-
-insert into inmate values(
-	1207523, 1, 2, 'Jessica', 'Freeborn', '1986-02-08', 2, '2025-11-15', '2026-02-15', 1, 1
-);
-
-insert into inmate values(
-	1180024, 2, 1, 'Christopher', 'Grant', '2005-10-01', 1, '2025-11-25', '2025-12-25', 1, 1
-);
-
-insert into inmate values(
-	5068, 1, 4, 'Jason', 'Garfield', '1979-12-31', 1, '2025-11-18', '9999-12-31', 1, 4
-);
-
-insert into inmate values(
-	55444, 3, 1, 'Michael', 'Hall', '1979-11-18', 1, '2025-05-21', '2026-05-21', 1, 1
-);
-
-insert into inmate values(
-	81325, 3, 1, 'Jim', 'Kauten', '1960-06-30', 1, '2025-11-13', '2027-11-13', 1, 1
-);
-
-insert into inmate values(
-	121201, 2, 2, 'Ava', 'Kock', '1960-01-09', 2, '2025-08-13', '2026-08-13', 1, 1
-);
-
-insert into inmate values(
-	1347693, 5, 1, 'Dylan', 'Leeper', '1993-08-08', 1, '2025-09-28', '2026-09-28', 1, 1
-);
-
-insert into inmate values(
-	31945, 5, 1, 'Marshawn', 'Levreston', '2000-12-13', 1, '2025-07-29', '2026-07-29', 1, 1
-);
-
-insert into inmate values(
-	7043, 6, 1, 'Thomas', 'McGann', '1977-04-01', 1, '2025-11-18', '2026-01-18', 1, 1
-);
-
-insert into inmate values(
-    163537, 2, 1, 'Andrew', 'Montgomery', '1998-03-14', 1, '2025-10-29', '2026-10-29', 1, 1
-);
-
-insert into inmate values(
-    4977, 2, 1, 'Craig', 'Neyers', '1973-07-22', 1, '2025-09-29', '2026-09-29', 1, 1
-);
-
-insert into inmate values(
-    118105, 3, 1, 'Matthew', 'Nguyen', '1985-02-16', 1, '2025-08-11', '2026-08-11', 1, 1
-);
-
-insert into inmate values(
-    123235, 3, 1, 'Adam', 'Pace', '1984-12-02', 1, '2025-10-21', '2026-10-21', 1, 1
-);
-
-insert into inmate values(
-    415883, 4, 1, 'Jonathan', 'Payton', '1985-09-11', 1, '2025-11-06', '2026-11-06', 1, 1
-);
-
-insert into inmate values(
-    61372, 1, 2, 'Erin', 'Roberts', '1998-05-25', 2, '2025-10-07', '2026-10-07', 1, 1
-);
-
-insert into inmate values(
-    361915, 5, 1, 'Chad', 'Schultz', '1975-03-09', 1, '2025-11-24', '2026-11-24', 1, 1
-);
-
-insert into inmate values(
-    334581, 6, 1, 'Troy', 'Spann', '1967-04-01', 1, '2025-11-19', '2026-11-19', 1, 1
-);
-
-insert into inmate values(
-    599909, 1, 2, 'Chelsea', 'Welker', '1993-10-17', 2, '2025-11-25', '2026-11-25', 1, 1
-);
-
+INSERT INTO `INMATE` VALUES
+	(58148, 1, 1, 'Anthony', 'Arreola', '1987-04-20', 1, '2025-08-23', '2025-08-23', 1, 1),
+    (376003, 1, 1, 'Benjamin', 'Blaschka', '1977-06-25', 1, '2025-11-21', '2026-11-21', 1, 1),
+    (5540, 1, 2, 'Trisha', 'Fenske', '1974-09-13', 2, '2025-11-25', '2026-11-25', 1, 1),
+    (1207523, 1, 2, 'Jessica', 'Freeborn', '1986-02-08', 2, '2025-11-15', '2026-02-15', 1, 1),
+    (1180024, 2, 1, 'Christopher', 'Grant', '2005-10-01', 1, '2025-11-25', '2025-12-25', 1, 1),
+    (5068, 1, 4, 'Jason', 'Garfield', '1979-12-31', 1, '2025-11-18', '9999-12-31', 1, 4),
+    (55444, 3, 1, 'Michael', 'Hall', '1979-11-18', 1, '2025-05-21', '2026-05-21', 1, 1),
+    (81325, 3, 1, 'Jim', 'Kauten', '1960-06-30', 1, '2025-11-13', '2027-11-13', 1, 1),
+    (121201, 2, 2, 'Ava', 'Kock', '1960-01-09', 2, '2025-08-13', '2026-08-13', 1, 1),
+    (1347693, 5, 1, 'Dylan', 'Leeper', '1993-08-08', 1, '2025-09-28', '2026-09-28', 1, 1),
+    (31945, 5, 1, 'Marshawn', 'Levreston', '2000-12-13', 1, '2025-07-29', '2026-07-29', 1, 1),
+    (7043, 6, 1, 'Thomas', 'McGann', '1977-04-01', 1, '2025-11-18', '2026-01-18', 1, 1),
+    (163537, 2, 1, 'Andrew', 'Montgomery', '1998-03-14', 1, '2025-10-29', '2026-10-29', 1, 1),
+    (4977, 2, 1, 'Craig', 'Neyers', '1973-07-22', 1, '2025-09-29', '2026-09-29', 1, 1),
+    (118105, 3, 1, 'Matthew', 'Nguyen', '1985-02-16', 1, '2025-08-11', '2026-08-11', 1, 1),
+    (123235, 3, 1, 'Adam', 'Pace', '1984-12-02', 1, '2025-10-21', '2026-10-21', 1, 1),
+    (415883, 4, 1, 'Jonathan', 'Payton', '1985-09-11', 1, '2025-11-06', '2026-11-06', 1, 1),
+    (61372, 1, 2, 'Erin', 'Roberts', '1998-05-25', 2, '2025-10-07', '2026-10-07', 1, 1),
+    (361915, 5, 1, 'Chad', 'Schultz', '1975-03-09', 1, '2025-11-24', '2026-11-24', 1, 1),
+    (334581, 6, 1, 'Troy', 'Spann', '1967-04-01', 1, '2025-11-19', '2026-11-19', 1, 1),
+	(599909, 1, 2, 'Chelsea', 'Welker', '1993-10-17', 2, '2025-11-25', '2026-11-25', 1, 1);
 
 create table `Offense`(
 	`offense_id` int primary key,
@@ -370,139 +308,41 @@ create table `Offense`(
     `sentence_length` INT GENERATED ALWAYS AS (TIMESTAMPDIFF(day, start_date, end_date)),
     FOREIGN KEY (inmate_id) references inmate(inmate_id)
 );
-
-insert into Offense(offense_id, inmate_id, charge, court_case_number, start_date, end_date, parole_eligible) values(
-	901234, 58148, 'Trespass-Return to Property Within One Year', '16088-1', '2025-08-23', '2026-08-23', true
-);
-
-insert into Offense(offense_id, inmate_id, charge, court_case_number, start_date, end_date, parole_eligible) values(
-	901235, 58148, 'Domestic Abuse - Violate Order for Protection', '16088-2', '2025-08-23', '2026-08-23', false
-);
-
-insert into Offense(offense_id, inmate_id, charge, court_case_number, start_date, end_date, parole_eligible) values(
-	901236, 58148, 'Drugs - 5th Degree - Possess Schedule 1,2,3,4', '16088-3', '2025-08-23', '2026-08-23', true
-);
-
-insert into Offense(offense_id, inmate_id, charge, court_case_number, start_date, end_date, parole_eligible) values(
-	901237, 58148, 'Domestic Assault-Felony', '16088-4', '2025-08-23', '2026-08-23', false
-);
-
-insert into Offense(offense_id, inmate_id, charge, court_case_number, start_date, end_date, parole_eligible) values(
-	901238, 58148, 'Obstruct Legal Process-Interfere w/Peace Officer', '16088-5', '2025-08-23', '2026-08-23', true
-);
-
-insert into Offense(offense_id, inmate_id, charge, court_case_number, start_date, end_date, parole_eligible) values(
-	901239, 58148, 'Harassment; Restraining Order - Violate Restriction', '16088-6', '2025-08-23', '2026-08-23', false
-);
-
-insert into Offense(offense_id, inmate_id, charge, court_case_number, start_date, end_date, parole_eligible) values(
-	901240, 58148, 'Disorderly Conduct', '16088-7', '2025-08-23', '2026-08-23', true
-);
-
-insert into Offense(offense_id, inmate_id, charge, court_case_number, start_date, end_date, parole_eligible) values(
-	901241, 58148, 'Assault-2nd Degree-Dangerous Weapon', '16088-8', '2025-08-23', '2026-08-23', false
-);
-
-insert into Offense(offense_id, inmate_id, charge, court_case_number, start_date, end_date, parole_eligible) values(
-	901242, 376003, 'Drugs - 5th Degree - Possess Schedule 1,2,3,4', '16345-1', '2025-11-21', '2026-11-21', true
-);
-
-insert into Offense(offense_id, inmate_id, charge, court_case_number, start_date, end_date, parole_eligible) values(
-	901244, 1207523, 'Traffic - DWI - Operate Motor Vehicle Under Influence', '16317-1', '2025-11-15', '2026-02-15', true
-);
-
-insert into Offense(offense_id, inmate_id, charge, court_case_number, start_date, end_date, parole_eligible) values(
-	901245, 1180024, 'Trespass-Occupy/Enter Dwelling/Locked/Posted', '16356-1', '2025-11-25', '2025-12-25', false
-);
-
-insert into Offense(offense_id, inmate_id, charge, court_case_number, start_date, end_date, parole_eligible) values(
-	901246, 55444, 'Possess Pornographic Work-Computer Disk/Electronic', '15786-1', '2025-05-21', '2026-05-21', true
-);
-
-insert into Offense(offense_id, inmate_id, charge, court_case_number, start_date, end_date, parole_eligible) values(
-	901247, 81325, 'Criminal Sexual Conduct-1st Degree-Penetration', '16318-1', '2025-11-13', '2026-11-13', false
-);
-
-insert into Offense(offense_id, inmate_id, charge, court_case_number, start_date, end_date, parole_eligible) values(
-	901248, 121201, 'Assault-2nd Degree-Dangerous Weapon', '16061-1', '2025-08-13', '2026-08-13', true
-);
-
-insert into Offense(offense_id, inmate_id, charge, court_case_number, start_date, end_date, parole_eligible) values(
-	901249, 121201, 'Murder - 2nd Degree - With Intent-Not Premeditated', '16061-2', '2025-08-13', '2026-08-13', false
-);
-
-insert into Offense(offense_id, inmate_id, charge, court_case_number, start_date, end_date, parole_eligible) values(
-	901250, 1347693, 'Aggravated Robbery-1st Degree', '16196-1', '2025-09-28', '2026-09-28', true
-);
-
-insert into Offense(offense_id, inmate_id, charge, court_case_number, start_date, end_date, parole_eligible) values(
-	901251, 31945, 'Trespass-Return to Property Within One Year', '16006-1', '2025-07-29', '2026-07-29', false
-);
-
-insert into Offense(offense_id, inmate_id, charge, court_case_number, start_date, end_date, parole_eligible) values(
-	901252, 31945, 'Damage to Property-4th Deg-Intentional Damage', '16006-2', '2025-07-29', '2026-07-29', true
-);
-
-
-insert into Offense(offense_id, inmate_id, charge, court_case_number, start_date, end_date, parole_eligible) values(
-	901254, 7043, 'Drugs - 5th Degree - Possess Schedule 1,2,3,4', '16337-1', '2025-11-18', '2026-01-18', true
-);
-
-insert into Offense(offense_id, inmate_id, charge, court_case_number, start_date, end_date, parole_eligible) values(
-	901255, 163537, 'Domestic Assault-Misdemeanor-Commits Act to Cause Fear', '16276-1', '2025-10-29', '2026-10-29', false
-);
-
-insert into Offense(offense_id, inmate_id, charge, court_case_number, start_date, end_date, parole_eligible) values(
-	901256, 163537, 'Threats of Violence - Display/exhibit/brandish', '16276-2', '2025-10-29', '2026-10-29', true
-);
-
-insert into Offense(offense_id, inmate_id, charge, court_case_number, start_date, end_date, parole_eligible) values(
-	901257, 163537, 'Assault-2nd Degree-Dangerous Weapon', '16276-3', '2025-10-29', '2026-10-29', false
-);
-
-insert into Offense(offense_id, inmate_id, charge, court_case_number, start_date, end_date, parole_eligible) values(
-	901258, 163537, 'False Imprisonment-Intentional Restraint', '16276-4', '2025-10-29', '2026-10-29', true
-);
-
-insert into Offense(offense_id, inmate_id, charge, court_case_number, start_date, end_date, parole_eligible) values(
-	901259, 4977, 'Drugs - 1st Degree - Possess 50 grams or more', '16199-1', '2025-09-29', '2026-09-29', false
-);
-
-insert into Offense(offense_id, inmate_id, charge, court_case_number, start_date, end_date, parole_eligible) values(
-	901260, 4977, 'Drugs - 1st Degree - Sale - 17 Grams or More', '16199-2', '2025-09-29', '2026-09-29', true
-);
-
-insert into Offense(offense_id, inmate_id, charge, court_case_number, start_date, end_date, parole_eligible) values(
-	901261, 118105, 'Assault - 4th Degree - Peace Officer - Throws Object', '16050-1', '2025-08-11', '2026-08-11', false
-);
-
-insert into Offense(offense_id, inmate_id, charge, court_case_number, start_date, end_date, parole_eligible) values(
-	901262, 123235, 'Domestic Abuse - Violate Order for Protection', '16258-1', '2025-10-21', '2026-10-21', true
-);
-
-insert into Offense(offense_id, inmate_id, charge, court_case_number, start_date, end_date, parole_eligible) values(
-	901263, 415883, 'Possess Ammo/Any Firearm - Conviction or Adjudication', '16300-1', '2025-11-06', '2026-11-06', false
-);
-
-insert into Offense(offense_id, inmate_id, charge, court_case_number, start_date, end_date, parole_eligible) values(
-	901264, 61372, 'Traffic - DWI - Operate Motor Vehicle - Alcohol', '16224-1', '2025-10-07', '2026-10-07', true
-);
-
-insert into Offense(offense_id, inmate_id, charge, court_case_number, start_date, end_date, parole_eligible) values(
-	901265, 361915, 'Drugs - 5th Degree - Possess Schedule 1,2,3,4', '16353-1', '2025-11-24', '2026-11-24', false
-);
-
-insert into Offense(offense_id, inmate_id, charge, court_case_number, start_date, end_date, parole_eligible) values(
-	901266, 334581, 'Predatory Offender - Knowingly commits act or omission', '16340-1', '2025-11-19', '2026-11-19', true
-);
-
-insert into Offense(offense_id, inmate_id, charge, court_case_number, start_date, end_date, parole_eligible) values(
-	901267, 599909, 'Theft-Take/Use/Transfer Movable Prop-No Consent', '16354-1', '2025-11-25', '2026-11-25', false
-);
-
-insert into Offense(offense_id, inmate_id, charge, court_case_number, start_date, end_date, parole_eligible) values(
-	901268, 599909, 'Drugs - 5th Degree - Possess Schedule 1,2,3,4', '16354-2', '2025-11-25', '2026-11-25', true
-);
+	
+insert into Offense(offense_id, inmate_id, charge, court_case_number, start_date, end_date, parole_eligible) values
+(901234, 58148, 'Trespass-Return to Property Within One Year', '16088-1', '2025-08-23', '2026-08-23', true),
+(901235, 58148, 'Domestic Abuse - Violate Order for Protection', '16088-2', '2025-08-23', '2026-08-23', false),
+(901236, 58148, 'Drugs - 5th Degree - Possess Schedule 1,2,3,4', '16088-3', '2025-08-23', '2026-08-23', true),
+(901237, 58148, 'Domestic Assault-Felony', '16088-4', '2025-08-23', '2026-08-23', false),
+(901238, 58148, 'Obstruct Legal Process-Interfere w/Peace Officer', '16088-5', '2025-08-23', '2026-08-23', true),
+(901239, 58148, 'Harassment; Restraining Order - Violate Restriction', '16088-6', '2025-08-23', '2026-08-23', false),
+(901240, 58148, 'Disorderly Conduct', '16088-7', '2025-08-23', '2026-08-23', true),
+(901241, 58148, 'Assault-2nd Degree-Dangerous Weapon', '16088-8', '2025-08-23', '2026-08-23', false),
+(901242, 376003, 'Drugs - 5th Degree - Possess Schedule 1,2,3,4', '16345-1', '2025-11-21', '2026-11-21', true),
+(901244, 1207523, 'Traffic - DWI - Operate Motor Vehicle Under Influence', '16317-1', '2025-11-15', '2026-02-15', true),
+(901245, 1180024, 'Trespass-Occupy/Enter Dwelling/Locked/Posted', '16356-1', '2025-11-25', '2025-12-25', false),
+(901246, 55444, 'Possess Pornographic Work-Computer Disk/Electronic', '15786-1', '2025-05-21', '2026-05-21', true),
+(901247, 81325, 'Criminal Sexual Conduct-1st Degree-Penetration', '16318-1', '2025-11-13', '2026-11-13', false),
+(901248, 121201, 'Assault-2nd Degree-Dangerous Weapon', '16061-1', '2025-08-13', '2026-08-13', true),
+(901249, 121201, 'Murder - 2nd Degree - With Intent-Not Premeditated', '16061-2', '2025-08-13', '2026-08-13', false),
+(901250, 1347693, 'Aggravated Robbery-1st Degree', '16196-1', '2025-09-28', '2026-09-28', true),
+(901251, 31945, 'Trespass-Return to Property Within One Year', '16006-1', '2025-07-29', '2026-07-29', false),
+(901252, 31945, 'Damage to Property-4th Deg-Intentional Damage', '16006-2', '2025-07-29', '2026-07-29', true),
+(901254, 7043, 'Drugs - 5th Degree - Possess Schedule 1,2,3,4', '16337-1', '2025-11-18', '2026-01-18', true),
+(901255, 163537, 'Domestic Assault-Misdemeanor-Commits Act to Cause Fear', '16276-1', '2025-10-29', '2026-10-29', false),
+(901256, 163537, 'Threats of Violence - Display/exhibit/brandish', '16276-2', '2025-10-29', '2026-10-29', true),
+(901257, 163537, 'Assault-2nd Degree-Dangerous Weapon', '16276-3', '2025-10-29', '2026-10-29', false),
+(901258, 163537, 'False Imprisonment-Intentional Restraint', '16276-4', '2025-10-29', '2026-10-29', true),
+(901259, 4977, 'Drugs - 1st Degree - Possess 50 grams or more', '16199-1', '2025-09-29', '2026-09-29', false),
+(901260, 4977, 'Drugs - 1st Degree - Sale - 17 Grams or More', '16199-2', '2025-09-29', '2026-09-29', true),
+(901261, 118105, 'Assault - 4th Degree - Peace Officer - Throws Object', '16050-1', '2025-08-11', '2026-08-11', false),
+(901262, 123235, 'Domestic Abuse - Violate Order for Protection', '16258-1', '2025-10-21', '2026-10-21', true),
+(901263, 415883, 'Possess Ammo/Any Firearm - Conviction or Adjudication', '16300-1', '2025-11-06', '2026-11-06', false),
+(901264, 61372, 'Traffic - DWI - Operate Motor Vehicle - Alcohol', '16224-1', '2025-10-07', '2026-10-07', true),
+(901265, 361915, 'Drugs - 5th Degree - Possess Schedule 1,2,3,4', '16353-1', '2025-11-24', '2026-11-24', false),
+(901266, 334581, 'Predatory Offender - Knowingly commits act or omission', '16340-1', '2025-11-19', '2026-11-19', true),
+(901267, 599909, 'Theft-Take/Use/Transfer Movable Prop-No Consent', '16354-1', '2025-11-25', '2026-11-25', false),
+(901268, 599909, 'Drugs - 5th Degree - Possess Schedule 1,2,3,4', '16354-2', '2025-11-25', '2026-11-25', true);
 
 create table `visit`(
 	`visit_id` int primary key,
